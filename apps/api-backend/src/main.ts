aae218e1fc3487e7968595a80f3ef5992379960c
Swasbuckler <shahandyhadian@gmail.com>
2025-08-24 23:26:14 +0700

    Initial Commit

---
+++ apps/api-backend/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 5000);
+  console.log("Server is listening to Port", process.env.PORT ?? 5000);
+}
+bootstrap();
