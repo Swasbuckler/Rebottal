b04db709eb4e4456132763442372d659a01ba5be
Swasbuckler <shahandyhadian@gmail.com>
2025-08-31 21:34:36 +0700

    Initialization of jwt Tokens

--- apps/api-backend/src/auth/auth.module.ts
+++ apps/api-backend/src/auth/auth.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [UsersModule]
+  imports: [
+    UsersModule,
+    JwtModule
+  ]
 })
 export class AuthModule {}
--- apps/api-backend/src/auth/auth.service.ts
+++ apps/api-backend/src/auth/auth.service.ts
@@ -3,26 +3,48 @@ import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
 import bcrypt from 'bcrypt';
 import { LogInUserDto } from 'src/users/dto/log-in-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private users: UsersService) {}
+  constructor(
+    private users: UsersService,
+    private jwt: JwtService
+  ) {}
 
   private readonly saltRounds = 10;
 
   async signUp(data: CreateUserDto) {
     const newUserData: CreateUserDto = JSON.parse(JSON.stringify(data));
 
-    await bcrypt.hash(data.password, this.saltRounds)
-      .then((hash) => {
-        newUserData.password = hash;
-      });
+    const hash = await bcrypt.hash(data.password, this.saltRounds)
+    newUserData.password = hash;
 
     await this.users.create(newUserData);
     return {status: HttpStatus.OK};
   }
 
   async logIn(data: LogInUserDto) {
+    const user = await this.validateUser(data);
+
+    const payload = { uuid: user.uuid, username: user.username };
+
+    const accessTokenExpiration = new Date(Date.now() + process.env.JWT_ACCESS_TOKEN_EXPIRATION!);
+    const refreshTokenExpiration = new Date(Date.now() + process.env.JWT_REFRESH_TOKEN_EXPIRATION!);
+
+    const accessToken = await this.jwt.signAsync(payload, {
+      secret: process.env.JWT_ACCESS_TOKEN_SECRET!,
+      expiresIn: `${process.env.JWT_ACCESS_TOKEN_EXPIRATION!}ms`
+    });
+
+    const refreshToken = await this.jwt.signAsync(payload, {
+      secret: process.env.JWT_REFRESH_TOKEN_SECRET!,
+      expiresIn: `${process.env.JWT_REFRESH_TOKEN_EXPIRATION!}ms`
+    });
+
+  }
+
+  async validateUser(data: LogInUserDto) {
     const user = await this.users.findUser(data.usernameOrEmail);
     
     if (!user) {
@@ -32,7 +54,7 @@ export class AuthService {
       throw new UnauthorizedException();
     }
 
-    const {password, ...result} = user!;
-
+    const {password, ...result} = user;
+    return result;
   }
 }
